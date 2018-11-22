85a2ca25ed33b9dccecde787f62f59f9d238075c
Anass Azeroual <azeroual.anass2015@gmail.com>
2018-11-22 16:55:53 +0000

    j'ajoute le ficheir personne.ts et je modifier sur les fichier home.component.html et home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Personne } from './Personne';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  employe = ["khalid" , "yassine"];
 
-  constructor() { }
+  personne:Personne;
+  personnes:Personne[];
+
+  prenom:string="zakaria";
+  voirPrenom:boolean=true;
+  nombre:number=0;
+  imageUrl="https://via.placeholder.com/150";
+  valide:boolean=false;
+
+  constructor() { 
+    this.personnes=[
+      {id : 1,nom: "azarwal",prenom:"anass"},
+      {id : 2,nom: "bouri",prenom:"mastafa"},
+      {id : 3,nom: "toumi",prenom:"zakaria"}
+    ]
+   }
 
   ngOnInit() {
   }
